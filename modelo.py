180ad89b2278e0560fe22c5a1c8fe4e4aa87f43f
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-04-05 19:20:58 -0300

    Implementing Dunder Methods for Playlist class Testing ABCs classes

--- modelo.py
+++ modelo.py
@@ -49,12 +49,7 @@ class Playlist:
     def __getitem__(self, item):
         return self._programas[item]
 
-    @property
-    def listagem(self):
-        return self._programas
-
-    @property
-    def tamanho(self):
+    def __len__(self):
         return len(self._programas)
 
 
---
+++ testeAbc.py
@@ -0,0 +1,7 @@
+from collections.abc import MutableSequence
+
+class MinhaListaMutavel(MutableSequence):
+    pass
+
+objetoValido = MinhaListaMutavel()
+print(objetoValido)
